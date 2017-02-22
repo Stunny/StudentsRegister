0818c7808378e1aa8d0f7ced36365a3dfd473fb6
Alex Vogel <avogelm@gmail.com>
2017-02-22 10:17:46 +0100

    Added a collapsing toolbar with parallax background image.

--- app/src/main/java/edu/salleurl/ls30394/studentsregister/MainActivity.java
+++ app/src/main/java/edu/salleurl/ls30394/studentsregister/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.salleurl.ls30394.studentsregister;
 
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewCompat.setTransitionName(findViewById(R.id.appBarLayout), "Transition");
+
+        CollapsingToolbarLayout ctl = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbar);
+        ctl.setTitle("Students Register");
+        ctl.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
     }
+
+
 }
